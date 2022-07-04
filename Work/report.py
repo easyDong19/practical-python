a7884c0f0cc6425aeb69406de04daa88afa2097f
easydong <ymh1353@naver.com>
2022-07-04 17:24:26 +0900

    third Coding #4.2~#5.2

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,8 @@
 # report.py
 
 import fileparse
+import stock
+import tableformat
 
 def read_portfolio(filename):
     '''
@@ -8,7 +10,9 @@ def read_portfolio(filename):
     name, shares, and price.
     '''
     with open(filename) as lines:
-        return fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
+        portdicts = fileparse.parse_csv(lines, select=['name','shares','price'], types=[str,int,float])
+        portfolio = [stock.Stock(d['name'],d['shares'],d['price'])for d in portdicts]
+        return portfolio
 
 def read_prices(filename):
     '''
@@ -30,29 +34,29 @@ def make_report_data(portfolio,prices):
         rows.append(summary)
     return rows
 
-def print_report(reportdata):
+def print_report(reportdata,formatter):
     '''
-    Print a nicely formated table from a list of (name, shares, price, change) tuples.
+    (name, shares, price, change) 튜플의 리스트로부터 보기 좋게 포매팅한 테이블을 프린팅.
     '''
-    headers = ('Name','Shares','Price','Change')
-    print('%10s %10s %10s %10s' % headers)
-    print(('-'*10 + ' ')*len(headers))
-    for row in reportdata:
-        print('%10s %10d %10.2f %10.2f' % row)
+    formatter.headings(['Name','Shares','Price','Change'])
+    for name, shares, price, change in reportdata:
+        rowdata = [ name, str(shares), f'{price:0.2f}', f'{change:0.2f}' ]
+        formatter.row(rowdata)
 
-def portfolio_report(portfoliofile, pricefile):
+def portfolio_report(portfoliofile, pricefile, fmt='txt'):
     '''
-    Make a stock report given portfolio and price data files.
+    주어진 포트폴리오와 가격 데이터 파일을 가지고 주식 보고서를 작성.
     '''
-    # Read data files
+    # 데이터 파일 읽기
     portfolio = read_portfolio(portfoliofile)
     prices = read_prices(pricefile)
 
-    # Create the report data
+    # 보고서 데이터 생성
     report = make_report_data(portfolio, prices)
 
-    # Print it out
-    print_report(report)
+    # 프린트
+    formatter = tableformat.create_formatter(fmt)
+    print_report(report, formatter)
 
 def main(args):
     if len(args) != 3:
---
+++ Work/test.py
@@ -0,0 +1,7 @@
+a = [1,9,4,25,16]
+
+i = a.__iter__()
+print(i.__next__())
+print(i.__next__())
+print(i.__next__())
+print(i.__next__())
